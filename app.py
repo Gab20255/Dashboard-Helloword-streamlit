a99d6498d464c1b12be12de82b98b6ffe5d33b9c
Gab2025 <gabrielodb23@gmail.com>
2026-02-06 15:06:10 -0300

    Primeiro commit do dashboard Streamlit

---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+
+
+
+st.set_page_config(
+    page_title="JRA - Gabriel Barboza",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+st.sidebar.title(" Navegação")
+st.sidebar.markdown("Selecione as opções de análise")
+
+arquivo = r'C:\Users\gabri\OneDrive\DesenvolvimentodeSoftware2\JRA\Semana01\Sreamlit\aula01\estudovendas_2024_03.parquet'
+
+st.sidebar.markdown("---")
+st.sidebar.markdown(" **Autor:** Gabriel Barboza")
+st.sidebar.markdown(" Projeto JRA")
+
+# ==============================
+# Cabeçalho principal
+# ==============================
+st.title("JRA - Gabriel Barboza")
+
+st.header("Streamlit + parquet + pandas")
+
+st.subheader("Análise de dados da empresa")
+
+st.markdown(
+    '''Análise de dados simples, utilizando dos arquivos **parquet** disponíveis.'''
+)
+
+st.markdown("---")
+
+# ==============================
+# Área principal de análise
+# ==============================
+st.markdown("## 🔎 Análise Exploratória")
+
+if arquivo:
+    df = pd.read_parquet(arquivo)
+
+    st.success("Arquivo carregado com sucesso!")
+
+    st.markdown("###  Estatísticas descritivas do arquivo Parquet estudovendas_2024_03.parquet")
+    st.dataframe(df.describe(), use_container_width=True)
+
+# Análise um sobre
+
+else:
+    st.info("Carregue um arquivo Parquet para iniciar a análise.")
+
+# ==============================
+# Rodapé
+# ==============================
+st.markdown("---")
+st.markdown(
+    "<center>© 2026 • Projeto JRA • Desenvolvido com Streamlit</center>",
+    unsafe_allow_html=True
+)
+
+
